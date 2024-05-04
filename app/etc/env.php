0f5b61ba139746349ed1a77e5d7a06b75073fa9c
RoyDanger <rayhansahrear@gmail.com>
2024-05-04 13:38:19 +0900

    Second commit

---
+++ app/etc/env.php
@@ -0,0 +1,89 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'FqOiCV7gOyOeedqyKanGDDr648Cteoub'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f73_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f73_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'config' => [
+        'async' => 0
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'base6458z4+8LcUhtYt1k0RKP4JdGKEau935b3A7IIx6rUIWY='
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2db',
+                'username' => 'magento2user',
+                'password' => 'Admin12345',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'graphql_query_resolver_result' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'rocktube.magento.com'
+    ],
+    'install' => [
+        'date' => 'Fri, 03 May 2024 08:06:24 +0000'
+    ]
+];
